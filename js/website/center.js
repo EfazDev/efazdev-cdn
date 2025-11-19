e4b8db558fb472ea770b2cb7dbb016f69e44c3b1
EfazDev <me@efaz.dev>
2025-11-18 19:50:16 -0600

    convert backticks

--- js/website/center.js
+++ js/website/center.js
@@ -105,71 +105,75 @@ async function GetPaidProjects() {
 
 document.addEventListener("DOMContentLoaded", () => {
     // Compile HTML
-    let topbar = generateHTMLDom(`<div class="topbar-ui">
-        <div class="topbar">
-            <div class="wrapper">
-                <div class="topbar-wrapper">
-                    <a rel="noopener noreferrer" class="link topbar-title-link" href="https://www.efaz.dev/"
-                        target="_blank"><img height="40" width="40" src="https://cdn.efaz.dev/png/logo.png">
-                        <p class="topbar-title">EfazDev</p>
-                    </a>
+    let topbar = generateHTMLDom(
+        '<div class="topbar-ui">' +
+            '<div class="topbar">' +
+                '<div class="wrapper">' +
+                    '<div class="topbar-wrapper">' +
+                        '<a rel="noopener noreferrer" class="link topbar-title-link" href="https://www.efaz.dev/" target="_blank">' +
+                            '<img height="40" width="40" src="https://cdn.efaz.dev/png/logo.png">' +
+                            '<p class="topbar-title">EfazDev</p>' +
+                        '</a>' +
 
-                    <div class="listOnRight topbar-buttons">
-                        <button onclick="home()">Home</button>
-                        <button onclick="redirectProjects()">Projects</button>
-                        <button onclick="redirectDashboard()">Dashboard</button>
-                        <button onclick="redirectAPIStatus()">API Status</button>
-                    </div>
-                    <button onclick="openPortraitMenu()" class="portrait-toggle">
-                        <img id="img" src="https://cdn.efaz.dev/svg/menu.svg">
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>`);
-    let portrait = generateHTMLDom(`
-        <div class="portrait-ui">
-            <div class="portrait-wrapper">
-                <a rel="noopener noreferrer" class="link portrait-title-link" href="https://www.efaz.dev/" target="_blank"><img height="40" width="40" src="https://cdn.efaz.dev/png/logo.png">
-                    <p class="portrait-title">EfazDev</p>
-                </a>
-                <div class="portrait-buttons">
-                    <button onclick="home()">Home</button>
-                    <button onclick="redirectProjects()">Projects</button>
-                    <button onclick="redirectDashboard()">Dashboard</button>
-                    <button onclick="redirectAPIStatus()">API Status</button>
-                    <button onclick="exitPortraitMenu()">Exit Menu</button>
-                </div>
-            </div>
-        </div>
-    `);
-    let footer = generateHTMLDom(`
-    <div class="footer" id="main_footer">
-    <br>
-        <a rel="noopener noreferrer" class="link footer-link">
-            <img height="40" width="40" src="https://cdn.efaz.dev/png/logo.png">
-            <p class="description">© Copyright 2025 EfazDev. All rights reserved</p>
-        </a>
-        <ul>
-            <button onclick="redirectPrivacy()" style="background-color: #ff4b00;" class="norm-button">
-                <img src="https://cdn.efaz.dev/png/orange_info_vector.png" style="float: left;" height="30px" width="30px"><span>Privacy Policy</span>
-            </button>
-            <button onclick="redirectTOS()" style="background-color: #ff4b00;" class="norm-button">
-                <img src="https://cdn.efaz.dev/png/orange_info_vector.png" height="30px" width="30px"><span>Terms of Service</span>
-            </button>
-            <button onclick="redirectTwitter()" style="background-color: #53b0fd;" class="norm-button">
-                <img src="https://cdn.efaz.dev/png/twitter_logo.png" height="30px" width="30px"><span>Twitter</span>
-            </button>
-            <button onclick="redirectDiscord()" style="background-color: #5c53fd;" class="norm-button">
-                <img src="https://cdn.efaz.dev/png/discord_logo.png" height="30px" width="30px"><span>Discord</span>
-            </button>
-            <button onclick="redirectYouTube()" style="background-color: #ff0000;" class="norm-button">
-                <img src="https://cdn.efaz.dev/png/youtube_logo.png" height="30px" width="30px"><span>YouTube</span>
-            </button>
-        </ul>
-        <br>
-    </div>
-    `);
+                        '<div class="listOnRight topbar-buttons">' +
+                            '<button onclick="home()">Home</button>' +
+                            '<button onclick="redirectProjects()">Projects</button>' +
+                            '<button onclick="redirectDashboard()">Dashboard</button>' +
+                            '<button onclick="redirectAPIStatus()">API Status</button>' +
+                        '</div>' +
+                        '<button onclick="openPortraitMenu()" class="portrait-toggle">' +
+                            '<img id="img" src="https://cdn.efaz.dev/svg/menu.svg">' +
+                        '</button>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>'
+    );
+    let portrait = generateHTMLDom('<div class="portrait-ui">' +
+        '<div class="portrait-wrapper">' +
+            '<a rel="noopener noreferrer" class="link portrait-title-link" href="https://www.efaz.dev/" target="_blank">' +
+                '<img height="40" width="40" src="https://cdn.efaz.dev/png/logo.png">' +
+                '<p class="portrait-title">EfazDev</p>' +
+            '</a>' +
+            '<div class="portrait-buttons">' +
+                '<button onclick="home()">Home</button>' +
+                '<button onclick="redirectProjects()">Projects</button>' +
+                '<button onclick="redirectDashboard()">Dashboard</button>' +
+                '<button onclick="redirectAPIStatus()">API Status</button>' +
+                '<button onclick="exitPortraitMenu()">Exit Menu</button>' +
+            '</div>' +
+        '</div>' +
+    '</div>');
+    let footer = generateHTMLDom('<div class="footer" id="main_footer">' +
+        '<br>' +
+        '<a rel="noopener noreferrer" class="link footer-link">' +
+            '<img height="40" width="40" src="https://cdn.efaz.dev/png/logo.png">' +
+            '<p class="description">© Copyright 2025 EfazDev. All rights reserved</p>' +
+        '</a>' +
+        '<ul>' +
+            '<button onclick="redirectPrivacy()" style="background-color: #ff4b00;" class="norm-button">' +
+                '<img src="https://cdn.efaz.dev/png/orange_info_vector.png" style="float: left;" height="30px" width="30px">' +
+                '<span>Privacy Policy</span>' +
+            '</button>' +
+            '<button onclick="redirectTOS()" style="background-color: #ff4b00;" class="norm-button">' +
+                '<img src="https://cdn.efaz.dev/png/orange_info_vector.png" height="30px" width="30px">' +
+                '<span>Terms of Service</span>' +
+            '</button>' +
+            '<button onclick="redirectTwitter()" style="background-color: #53b0fd;" class="norm-button">' +
+                '<img src="https://cdn.efaz.dev/png/twitter_logo.png" height="30px" width="30px">' +
+                '<span>Twitter</span>' +
+            '</button>' +
+            '<button onclick="redirectDiscord()" style="background-color: #5c53fd;" class="norm-button">' +
+                '<img src="https://cdn.efaz.dev/png/discord_logo.png" height="30px" width="30px">' +
+                '<span>Discord</span>' +
+            '</button>' +
+            '<button onclick="redirectYouTube()" style="background-color: #ff0000;" class="norm-button">' +
+                '<img src="https://cdn.efaz.dev/png/youtube_logo.png" height="30px" width="30px">' +
+                '<span>YouTube</span>' +
+            '</button>' +
+        '</ul>' +
+        '<br>' +
+    '</div>');
 
     // Add All Base Elements
     document.body.prepend(portrait);
